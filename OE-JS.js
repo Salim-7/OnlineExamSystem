4dfa975d7b11e88eb3916cc2adc50d72c5f88325
salim-7 <salim200904@gmail.com>
2017-03-15 15:54:36 +0200

    first

---
+++ OE-JS.js
@@ -0,0 +1,42 @@
+﻿
+
+$(document).ready(function (){
+
+    $('#btnLogin').click(function () {
+    
+    
+        $.ajax({
+
+            url: '/token',
+            method: 'POST',
+            contentType:'appliaction/json',
+            data: {
+
+                UserName: $('#TextUserName').val(),
+                PassWord: $('#TextPassWord').val(),
+                grant_type:'password',
+            },
+            success: function (response) {
+
+                sessionStorage.setItem('accessToken', response.access_token);
+                window.location.href = "data.html"
+
+            },
+            error: function (jqXHR) {
+                $('#divErrorText').text(jqXHR.responseText);
+                $('#divError').show('fade');
+            }
+
+
+
+        });
+    
+    
+    
+    
+    });
+
+
+
+
+};
